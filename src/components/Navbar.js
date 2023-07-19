00bd1403aba75afa8d5ce87ac57034ec97165c14
educarmas <edcarrasmar@gmail.com>
2023-07-19 19:15:51 +0200

    creación y borrado carpetas

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../components/Navbar.css'
+
+
+// Menú Hamburguesa Móvil
+
+// const toggleButton = document.querySelector("#button-menu");
+// const navWrapper = document.querySelector("#nav");
+
+// toggleButton.addEventListener("click", () => {
+//     toggleButton.classList.toggle("close");
+//     navWrapper.classList.toggle("show");
+// });
+
+// navWrapper.addEventListener("click", (e) => {
+//     if (e.target.id === "nav") {
+//         navWrapper.classList.remove("show");
+//         toggleButton.classList.remove("close");
+//     }
+// });
