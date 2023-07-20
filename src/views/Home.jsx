49bded0b11402479a339a584114c90201d2a0259
educarmas <edcarrasmar@gmail.com>
2023-07-20 14:59:28 +0200

    merge navbar con develop

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,21 +1,24 @@
 import dummyData from '../context/dummy-data.json';
 import Productcard from '../components/productcard/Productcard';
+import Navbar from '../components/Header/Navbar';
+
 
 export default function Home() {
     let courseData = dummyData.courses;
 
-    return(
+    return (
         <>
-            {courseData && courseData.map((course,index)=>{
-                return(
+            <Navbar />
+            {courseData && courseData.map((course, index) => {
+                return (
                     <Productcard key={index}
-                    img={course.photo} 
-                    level={course.level} 
-                    title={course.title} 
-                    price={course.price} >
+                        img={course.photo}
+                        level={course.level}
+                        title={course.title}
+                        price={course.price} >
                     </Productcard>
                 )
-            })} 
+            })}
         </>
     )
 }
