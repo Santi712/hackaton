3e6bec339941e432c8e12a7d70537ef7737a0385
Rocio <rocioalonso.dev@gmail.com>
2023-07-20 13:38:38 +0200

    working on router

--- src/config/Router.jsx
+++ src/config/Router.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../views/Home";
+import Course from '../views/Course';
 
 export const Router = () => {
+
+
     return(
        <BrowserRouter>
             <Routes>
-                <Route path="/" element={<LandingPage/>} ></Route>
-                <Route path="/curso" element={<Course/>}></Route>
+                <Route path="/" element={<Home/>} ></Route>
+                <Route path="Courses/:id" element={<Course/>}></Route>
             </Routes>
        </BrowserRouter>
     )
---
+++ src/views/Home.jsx
@@ -0,0 +1,21 @@
+
+// import { Link } from 'react-router-dom';
+
+export default function Home() {
+
+
+    return(
+        <>
+            {/* {courseData && courseData.map((course,index)=>{
+                return(
+                    <Link to={`/curso/:id}`} key={index}>
+                        <p>{course.title}</p>
+                    </Link>
+                    
+                )
+            })} */}
+            
+        </>
+    )
+}
+
