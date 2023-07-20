4acfa8a877c0a29d30555362f0703e3bf562321d
Rocio <rocioalonso.dev@gmail.com>
2023-07-20 14:01:09 +0200

    added producto links to home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,19 +1,20 @@
-
-// import { Link } from 'react-router-dom';
+import dummyData from '../context/dummy-data.json';
+import { Link } from 'react-router-dom';
+// import Productcard from '../components/productcard/Productcard';
 
 export default function Home() {
-
-
+    let courseData = dummyData.courses;
+    console.log(courseData)
     return(
         <>
-            {/* {courseData && courseData.map((course,index)=>{
+            {courseData && courseData.map((course,index)=>{
                 return(
-                    <Link to={`/curso/:id}`} key={index}>
+                    <Link to={`/curso/${index}`} key={index}>
                         <p>{course.title}</p>
                     </Link>
-                    
                 )
-            })} */}
+            })}
+            
             
         </>
     )
