5f8692f4442d39ffd49e8f71fe6869af56db33cc
Rocio <rocioalonso.dev@gmail.com>
2023-07-20 14:17:44 +0200

    inserted productcard in home

--- src/components/productcard/Productcard.jsx
+++ src/components/productcard/Productcard.jsx
@@ -1,23 +1,29 @@
 import "./Productcard.css"
 import React from 'react'
-
-import image1 from "../../assets/image/imagen-curso1.png"
 import image2 from "../../assets/image/play pause.png"
 
-export default function Productcard() {
+export default function Productcard(props) {
     return(
         
         <div className="container-productcard">
-           <div className="container-image">
-           <img alt="imagen curso" src={image1}></img>
-           <div className="container-text-level"><p className="level-text">principiante</p></div>
-           
+            <div className="container-image">
+                <img alt="imagen curso" src={props.img}></img>
+                <div className="container-text-level">
+                        <p className="level-text">{props.level}</p>
+                </div>
+            </div>
+            <div className="container-text">
+                <p>{props.title}</p>
+            </div>
+            <div className="container-price-buy">
+                <div className="container-price">
+                    <p>{props.price}</p>
+                </div>
+                <div className="container-buy">
+                    <p>Comprar&nbsp;&nbsp;<img alt="imagen play" src={image2}></img></p>
+                </div>
+            </div>
         </div>
-        <div className="container-text"><p>Figma paso a paso</p></div>
-        <div className="container-price-buy">
-        <div className="container-price"><p>25,99€</p></div>
-        <div className="container-buy"><p>Comprar&nbsp;&nbsp;<img alt="imagen play" src={image2}></img></p></div>
-        </div></div>
         
     )
 }
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,21 +1,21 @@
 import dummyData from '../context/dummy-data.json';
-import { Link } from 'react-router-dom';
-// import Productcard from '../components/productcard/Productcard';
+import Productcard from '../components/productcard/Productcard';
 
 export default function Home() {
     let courseData = dummyData.courses;
-    console.log(courseData)
+
     return(
         <>
             {courseData && courseData.map((course,index)=>{
                 return(
-                    <Link to={`/curso/${index}`} key={index}>
-                        <p>{course.title}</p>
-                    </Link>
+                    <Productcard key={index}
+                    img={course.photo} 
+                    level={course.level} 
+                    title={course.title} 
+                    price={course.price} >
+                    </Productcard>
                 )
-            })}
-            
-            
+            })} 
         </>
     )
 }
