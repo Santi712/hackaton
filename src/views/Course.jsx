cc1faa9c682c6a7700400274d62fe158360b4deb
Sylvia Llorente <sylviall81@gmail.com>
2023-07-20 17:58:02 +0200

    cambios en json

--- src/views/Course.jsx
+++ src/views/Course.jsx
@@ -1,5 +1,11 @@
+import ProductDetail from '../components/productDetail2/productDetail'
+
+
 export default function Course({courseData}) {
     return(
-        <h1>{courseData.title}</h1>
+        
+        /*<h1>{courseData.title}</h1>*/
+
+        <ProductDetail photoUrl = {courseData.photo} level={courseData.level} title = {courseData.title} shortDescription = {courseData.shortDescription} price = {courseData.price} discountPrice ={courseData.discountPrice} position={courseData.owner.position} name= {courseData.owner.name} experience={courseData.experience}/>
     )
 }
