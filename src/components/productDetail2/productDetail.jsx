13881283373724b0b25bdadbfab75fc5574cfdd9
Sylvia Llorente <sylviall81@gmail.com>
2023-07-20 13:07:37 +0200

    nvo componente prueba

---
+++ src/components/productDetail2/productDetail.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './ProductDetail.css'
+
+
+const ProductDetail = (props) => 
+{
+    
+    return (
+        <div className = "productDetail-container">
+    
+            <div className = "productDetail-courseCard">
+                
+                <div className = "courseCard-leftDiv">
+
+                    <img src = {props.photoUrl} alt = "logotipo del curso ofrecido" className= "courseLogoImage"/>
+                    <button className = "level-tag">{props.level}</button>
+                </div>
+
+                <h3>{props.title}</h3>                   
+             <p>{props.shortDescription}</p>
+                    <h4>{props.price}</h4>
+                    <h4>{props.discountPrice}</h4>
+            </div>
+    
+        </div>
+    )}
+export default ProductDetail;
