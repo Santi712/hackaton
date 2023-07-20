7f9834714dc6adbfef83dfaffd149184b10ac34e
Sylvia Llorente <sylviall81@gmail.com>
2023-07-20 16:54:26 +0200

    componente mobile css

--- src/components/productDetail2/productDetail.jsx
+++ src/components/productDetail2/productDetail.jsx
@@ -11,25 +11,27 @@ const ProductDetail = (props) =>
             <div className = "productDetail-courseCard">
                     <img src = {props.photoUrl} alt = "logotipo del curso ofrecido" className= "courseLogoImage"/>
                     <button className = "level-tag">{props.level}</button>
-                
+                   <div className = "texto-cursos">
                 <h3>{props.title}</h3>                   
                 <p>{props.shortDescription}</p>
                 
                     <h4 className = "discounted-price">{props.price}</h4>
                     <h3 className = "price">{props.discountPrice}</h3>
-
+                    </div>
 <div className = "teacher-info">
     <div className = "round-photo"></div>
+    <div className = "teacher-text-container">
                     <h4 className = "teacher-name">{props.name}</h4>
-                    <h5 classNname = "teacher-position">{props.position}</h5>
+                    <h5 className = "teacher-position">{props.position}</h5>
                     </div>
-            </div>
-
+                    </div>
+    <div className = "experience-text">
 <h4>¿Que incluye la experiencia?</h4>
-<h5>Lorem Ipsum dolor sit amet Lorem ipsum dolor sit amet.</h5>
-
-            <button className = "cart-button">Añadir al carrito</button>
-    
+<h5 className= "experience-description">Lorem Ipsum dolor sit amet Lorem ipsum dolor sit amet.</h5>
+</div>
+            <button className = "shoppingcart-button">Añadir al carrito</button>
+           
+            </div>
         </div>
     )}
 export default ProductDetail;
